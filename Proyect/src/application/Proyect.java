a171a50c77024e86fccc37d7cdaadd0cc57c1559
qjot <mr.lorak@gmail.com>
2016-04-14 15:10:50 +0200

    Change proyect structure <model,contoller,view>

--- Proyect/src/application/Proyect.java
+++ Proyect/src/application/Proyect.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyect;
+package application;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class Proyect extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
         
