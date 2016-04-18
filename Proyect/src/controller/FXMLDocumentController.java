73b25b3056b224e641bf634af0eb05e09b9854f0
qjot <mr.lorak@gmail.com>
2016-04-18 17:03:54 +0200

    First simple database use

--- Proyect/src/controller/FXMLDocumentController.java
+++ Proyect/src/controller/FXMLDocumentController.java
@@ -5,12 +5,20 @@
  */
 package controller;
 
+import es.upv.inf.Database;
+import es.upv.inf.Product;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import model.PC;
 
 /**
  *
@@ -20,6 +28,10 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private ListView ramListView;
+    private ObservableList<String> data = null;
+    
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -29,6 +41,20 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        
+        Database db = new Database(); //TODO singleton
+        PC defaultPc = new PC();
+        List<Product> ramList;
+        ramList = Database.getProductByCategory(Product.Category.RAM);
+        ArrayList<String> myData = new ArrayList<String>();
+        for(int i = 0; i < ramList.size();i++)
+        {
+            myData.add(ramList.get(i).getDescription());
+        }
+        
+        data = FXCollections.observableArrayList(myData);
+        ramListView.setItems(data);
+        
         // TODO
     }    
     
