4da0c4c5595d485eabc8439e36579972e10b781f
qjot <mr.lorak@gmail.com>
2016-04-19 23:19:14 +0200

    Działające funkcje

--- Proyect/src/controller/MainWindowFXMLController.java
+++ Proyect/src/controller/MainWindowFXMLController.java
@@ -5,8 +5,12 @@
  */
 package controller;
 
+import java.io.Console;
 import java.io.IOException;
+import static java.lang.System.console;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -14,6 +18,9 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import model.PC;
 
 /**
  * FXML Controller class
@@ -22,6 +29,22 @@ import javafx.scene.control.Button;
  */
 public class MainWindowFXMLController implements Initializable {
 
+        @FXML
+        private AnchorPane contentMainView;
+        private List<PC> pcList = new ArrayList<>();
+   
+        public void InitController()
+        {
+                  try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/FXMLDocument.fxml"));
+            Parent root = (Parent) loader.load();
+            FXMLDocumentController pcController = loader.<FXMLDocumentController>getController();
+            pcList= pcController.GetPcList();
+         
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
     
     @FXML
     private void ShowDefaultPc(ActionEvent event) {
@@ -31,11 +54,63 @@ public class MainWindowFXMLController implements Initializable {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/FXMLDocument.fxml"));
             Parent root = (Parent) loader.load();
             FXMLDocumentController pcController = loader.<FXMLDocumentController>getController();
+           // delete pcList= pcController.GetPcList();
             pcController.ShowDefaultPc(pcName.toUpperCase());
         } catch (IOException e) {
             e.printStackTrace();
         }
         }
+    
+        public void ShowDefaultPc(String pcName)
+    {       
+        for (int i=0; i<pcList.size();i++)
+        {
+            String tempString = pcList.get(i).getPcName();
+            if(tempString.equals(pcName))
+            {
+                GoToPcDesc(pcList.get(i));
+                break;
+            }
+                  
+        }
+    }
+        
+          public void GoToPcDesc(PC pc)
+    {
+           try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PcDescriptionFXML.fxml"));
+            Parent root = (Parent) loader.load();
+            PcDescriptionFXMLController pcController = loader.<PcDescriptionFXMLController>getController();
+            ChangeContent(root);
+            pcController.initController(pc);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+               
+        private void ChangeContent(Parent loader) {
+        Stage search = new Stage();
+        AnchorPane aPane = (AnchorPane) loader;
+        aPane.autosize(); 
+        contentMainView.getChildren().clear();
+        contentMainView.getChildren().addAll(aPane.getChildren());
+    }
+            @FXML
+    private void GoToPcDescription(ActionEvent event) {
+        try {
+            String pcName = event.getSource().toString();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PcDescriptionFXML.fxml"));
+            Parent root = (Parent) loader.load();
+            PcDescriptionFXMLController pcController = loader.<PcDescriptionFXMLController>getController();
+            ChangeContent(root);
+            pcController.initController(pcList.get(0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        }
+    
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
