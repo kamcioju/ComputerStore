58f01366346b839ec4f863111b56c2b09a064002
qjot <mr.lorak@gmail.com>
2016-04-20 23:41:45 +0200

    Add To Cart Done

--- Proyect/src/model/CartPC.java
+++ Proyect/src/model/CartPC.java
@@ -10,34 +10,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *a
+ * a
+ *
  * @author qjot
  */
 public class CartPC {
 
     public static PC currentPC = new PC();
 
-    public static boolean addProduct(Product product) {
+    public static String addProduct(Product product) {
         Product.Category cat = product.getCategory();
         String catString = product.getCategory().toString();
         List<Product> productCategoryList = currentPC.GetProductsByCategory(cat);
-        if(CheckAvailability(productCategoryList, catString))
-        {
-            currentPC.addProduct(product);
-            return true;
+        int checkMessage = CheckAvailability(productCategoryList, product, catString);
+        switch(checkMessage){
+            
+            case 1: return "Stock is empty.";
+            case 2: return "You can not add more this type of product.";
+            case 3: currentPC.addProduct(product); 
+                    return "Product added to cart!";
+            
         }
-        return false;
+        return "Try again";
     }
-    public static boolean CheckAvailability(List<Product> productList, String catString)
-    {
-                if (!productList.isEmpty()) {
+
+    public static int CheckAvailability(List<Product> productList, Product product, String catString) {
+        int count = 0;
+        for (Product thisProduct : productList) {
+            if (thisProduct.equals(product)) {
+                count++;
+            }
+        }
+        if (product.getStock() <= count) {
+            return 1;
+        }
+        if (!productList.isEmpty()) {
             //SPEAKER, HDD, HDD_SSD, POWER_SUPPLY, DVD_WRITER, RAM, SCREEN, MULTIREADER, MOTHERBOARD, CPU, MOUSE, GPU, KEYBOARD, CASE, FAN
             switch (catString) {
                 case "MOTHERBOARD":
                 case "CPU":
                 case "CASE":
                 case "POWER_SUPPLY":
-                    return false;
+                    return 2; //full cart
                 case "HDD":
                 case "HDD_SDD":
                 case "RAM":
@@ -52,18 +66,17 @@ public class CartPC {
 
             }
 
-        }
-        else 
-        {
-            return true;
-        }
-        return false;
-    }
-    public static boolean CheckQuantiy(List<Product> productList, int quantity) {
-        if (productList.size() < quantity) {
-            return true;
         } else {
-            return false;
+            return 3; //You can add product
+        }
+        return 2; //full cart
+    }
+
+    public static int CheckQuantiy(List<Product> productList, int quantity) {
+        if (productList.size() < quantity) {
+            return 3; //You can add product
+        } else {
+            return 2; //full cart
         }
     }
 }
