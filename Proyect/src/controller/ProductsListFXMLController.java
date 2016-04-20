b8509f49e607b4fc85aba622f5e27e891a9ed930
kaqq <kaqq@192.168.1.35>
2016-04-20 02:03:13 +0200

    suwaczek wersja idealna

--- Proyect/src/controller/ProductsListFXMLController.java
+++ Proyect/src/controller/ProductsListFXMLController.java
@@ -5,6 +5,8 @@
  */
 package controller;
 
+import es.upv.inf.Database;
+import static es.upv.inf.Database.getProductByCategoryAndPrice;
 import es.upv.inf.Product;
 import java.io.IOException;
 import java.net.URL;
@@ -14,6 +16,7 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -22,9 +25,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
-
+import org.controlsfx.control.RangeSlider;
 
 
 
@@ -39,18 +45,66 @@ public class ProductsListFXMLController implements Initializable {
     @FXML private TableColumn<Product, Integer> priceColumn;
     @FXML private TableColumn<Product, Integer> quantityColumn;
     @FXML private TableColumn<Product, String> descriptionColumn; 
+    @FXML private Button showButton;
+    @FXML private VBox searchBar;
     private ObservableList<Product> product_list = FXCollections.observableArrayList();
     private List<Product> productList;
     private Product selectedProduct;
-    @FXML
-    private Rectangle Bottom;
-    @FXML
-    private Button showButton;
+    Product.Category category;
+    RangeSlider hSlider;
+   
     
+    
+    public void setupSlider(ObservableList<Product>  p_list){
+        
+        //setup minimum and maximum
+        double minimumPrice=p_list.get(0).getPrice();
+        double maximumPrice=p_list.get(0).getPrice();
+        category =p_list.get(0).getCategory();
+        for(Product p: p_list)
+        {
+            
+            if(p.getPrice()<minimumPrice)
+            {minimumPrice=p.getPrice();}
+            if(p.getPrice()>maximumPrice)
+            {maximumPrice=p.getPrice();}
+        }
+                  RefreshList(category,minimumPrice,minimumPrice);
+        hSlider = new RangeSlider(minimumPrice, maximumPrice, minimumPrice, maximumPrice);
+        hSlider.setShowTickMarks(true);
+        hSlider.setShowTickLabels(true);
+        hSlider.setBlockIncrement(1);
+        searchBar.getChildren().add(hSlider);
+        hSlider.highValueProperty().addListener((observable, oldValue, newValue) -> {
+        product_list.clear();
+        for(Product p: productList)
+        {
+            if(p.getPrice()<=newValue.doubleValue())
+                product_list.add(p);
+        }
+        productsTableView.sort();
+     // product_list.addAll(Database.getProductByCategoryAndPrice(category, minimumPrice, maximumPrice, true));
+});
+         hSlider.lowValueProperty().addListener((observable, oldValue, newValue) -> {
+         product_list.clear();
+             for(Product p: productList)
+        {
+        if(p.getPrice()>=newValue.doubleValue())
+                product_list.add(p);
+        }
+             productsTableView.sort();
+     // product_list.addAll(Database.getProductByCategoryAndPrice(category, minimumPrice, maximumPrice, true));
+});
+    }
+    public void RefreshList(Product.Category category,double minimumPrice,double maximumPrice){
+      
+      
+    }
     public void initController(List<Product> p_list)
     {
         this.productList = p_list;
         product_list.addAll(p_list);
+        setupSlider(product_list);
         productsTableView.setItems(product_list);
         //this.currentPc = pc;    
        //addComponentsToTableView(product_list)
@@ -62,6 +116,7 @@ public class ProductsListFXMLController implements Initializable {
         {
         selectedProduct = productsTableView.getSelectionModel().getSelectedItem();
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/FXMLDocument.fxml"));
+        
         Parent root = (Parent) loader.load();
         FXMLDocumentController appController = loader.<FXMLDocumentController>getController();
         appController.GoToProductDescription(selectedProduct);
@@ -78,6 +133,8 @@ public class ProductsListFXMLController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+       
+        
         
         descriptionColumn.prefWidthProperty().bind(productsTableView.widthProperty().divide(2).subtract(10));
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<Product, String>("description"));
